6904dc4189671845926cf5c9e99b532a89847a33
Pawel Damasiewicz <pawel.damasiewicz@gmail.com>
2012-03-30 07:19:31 -0400

    deleting *'s words

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,5 @@
-require 'gosu'
-file = "/usr/share/dict/words"
+ #!/usr/bin/env ruby -wKU
+
+words = File.open("/usr/share/dict/words" ).read.split("\n") 
+words.delete_if{|word| word =~ /[a-zA-Z]*\'s/}
+p words[4]
